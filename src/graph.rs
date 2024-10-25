30b0d1354077ec658dc8b1b983d1ce4ec491157a
jakob753951 <jlm1999@live.dk>
2024-10-25 21:40:12 +0200

    Small update. Might break stuff

--- src/graph.rs
+++ src/graph.rs
@@ -24,12 +24,39 @@ impl<VId, V> Graph<VId, V>
     pub fn add_vertex(&mut self, vertex: VId, value: V) {
         self.vertices.insert(vertex, value);
     }
+}
 
-    pub fn add_predecessor(&mut self, from: VId, to: VId) {
-        self.predecessor_edges.entry(from).or_default().insert(to);
+impl<VId, V> Graph<VId, V>
+where
+    VId: Eq + Hash + Clone,
+    V: Hash + Clone,
+{
+    pub fn is_acyclic(&self) -> bool {
+        let Some(first_node) = self.vertices.iter().next() else {
+            return true;
+        };
+
+        let all_nodes = self.vertices.clone().into_keys().collect::<HashSet<VId>>();
+
+        self.connected_nodes(first_node.0.clone(), HashSet::new()) == all_nodes
     }
-    pub fn add_successor(&mut self, from: VId, to: VId) {
-        self.successor_edges.entry(from).or_default().insert(to);
+
+    fn connected_nodes(&self, id: VId, visited_nodes: HashSet<VId>) -> HashSet<VId> {
+        if visited_nodes.contains(&id) {
+            return visited_nodes;
+        }
+        // add id to visited_nodes
+        let visited_nodes: HashSet<VId> = visited_nodes.union(&HashSet::from([id.clone()])).cloned().collect();
+
+        let default_set = HashSet::new();
+        let predecessor_ids = self.predecessor_edges.get(&id).unwrap_or(&default_set);
+        let default_set = HashSet::new();
+        let successor_ids = self.successor_edges.get(&id).unwrap_or(&default_set);
+        let directly_connected_nodes = predecessor_ids.union(&successor_ids);
+
+        directly_connected_nodes
+            .map(|node| self.connected_nodes(node.clone(), visited_nodes.clone()))
+            .fold(HashSet::new(), |a, b| a.union(&b).cloned().collect())
     }
 }
 
@@ -39,7 +66,31 @@ impl<VId, V> Graph<VId, V>
         V: Hash,
 {
     pub fn connect_vertices(&mut self, from: VId, to: VId) {
-        self.add_successor(from.clone(), to.clone());
-        self.add_predecessor(to, from);
+        self.successor_edges.entry(from.clone()).or_default().insert(to.clone());
+        self.predecessor_edges.entry(to).or_default().insert(from);
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn normal_graph_works() {
+        let mut graph = Graph::new();
+
+        graph.add_vertex("a", 1);
+        graph.add_vertex("b", 2);
+        graph.add_vertex("c", 3);
+        graph.add_vertex("d", 4);
+
+        graph.connect_vertices("a", "b");
+        graph.connect_vertices("a", "c");
+        graph.connect_vertices("b", "d");
+        graph.connect_vertices("c", "d");
+
+        assert!(graph.is_acyclic());
+
+        assert_eq!(graph.connected_nodes("a", HashSet::new()), HashSet::from(["a", "b", "c", "d"]));
     }
 }
